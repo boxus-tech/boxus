45b003966cd11f84cb85534f7e7db320805dfc62
Alex K <alex.lumir@gmail.com>
2017-06-18 15:39:11 +0300

    Config files for DB class + defaults

--- boxus/db.py
+++ boxus/db.py
@@ -1,18 +1,42 @@
+import yaml
+
 from couchdb.client import Server
 
 class DB:
 
-    server = None
-    sensors = None
-    measurements = None
+    server =    None
+    sensors =   None
+    readings =  None
+    devices =   None
+    
+    config = {
+        'server': {
+          'host':   'localhost',
+          'port':   5984
+        },
 
-    def __init__(self):
+        'schema': {
+          'sensors_db':     'sensors',
+          'readings_db':    'readings',
+          'devices_db':     'devices'
+        }
+    }
+
+    def __init__(self, config_path = None):
         self.server = Server()
 
+        if config_path:
+            self.config = yaml.load(open(config_path, 'r').read())
+
     def connect(self):
-        self.sensors = self.server['sensors']
-        self.measurements = self.server['measurements']
+        self.sensors    = self.server[self.config['schema']['sensors_db']]
+        self.readings   = self.server[self.config['schema']['readings_db']]
+        self.devices    = self.server[self.config['schema']['devices_db']]
 
     def setup(self):
-        self.server.create('sensors')
-        self.server.create('measurements')
+        if self.config['schema']['sensors_db'] not in self.server:
+            self.server.create(self.config['schema']['sensors_db'])
+        if self.config['schema']['readings_db'] not in self.server:
+            self.server.create(self.config['schema']['readings_db'])
+        if self.config['schema']['devices_db'] not in self.server:
+            self.server.create(self.config['schema']['devices_db'])
