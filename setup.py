8547776cf59e3e02e7edf0cc8e3d026e0189bf1b
Alex K <alex.lumir@gmail.com>
2017-06-22 23:47:28 +0300

    Pypi url updated

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='boxus',
       version='0.0.2',
       description='Open automated plants grow pod project',
-      url='https://github.com/ololobus/boxus',
+      url='https://github.com/boxus-plants/boxus',
       author='Alexey Kondratov',
       author_email='kondratov.aleksey@gmail.com',
       license='GNU General Public License v3.0',
