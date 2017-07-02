4dd91d98128d985b7be45c67fab402711ba2d6d9
Alex K <alex.lumir@gmail.com>
2017-07-02 15:52:49 +0300

    Device on_for N seconds method added

--- boxus/device.py
+++ boxus/device.py
@@ -1,3 +1,4 @@
+import time
 from couchdb.mapping import TextField
 
 from .controllable import Controllable
@@ -17,6 +18,29 @@ class Device(Controllable):
 
     def off(self):
         return self._send_control_sequence('off', self.type_name, False)
+
+    def on_for(self, val, units='seconds'):
+        assert units in [
+                'second', 'seconds',
+                'minute', 'minutes',
+                'hour', 'hours',
+                'day', 'days'
+            ]
+
+        delay = 0 # seconds
+
+        if units in ['second', 'seconds']:
+            delay = val
+        elif units in ['minute', 'minutes']:
+            delay = val*60
+        elif units in ['hour', 'hours']:
+            delay = val*60*60
+        elif units in ['day', 'days']:
+            delay = val*60*60*24
+
+        self.on()
+        time.sleep(delay)
+        self.off()
 
     def _on_generic(self):
         self.digital_out(self.pins['power']['number'], 1)
